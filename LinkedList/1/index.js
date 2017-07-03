5eb1093fa202c6e249b1b70bc8482e2c59bcd1d1
r.seleznev <r.seleznev@sis-it.pro>
2017-07-03 17:13:56 +0700

    LinkedList

---
+++ LinkedList/1/index.js
@@ -0,0 +1,81 @@
+class Link {
+	constructor(id, data) {
+		this.id = id;
+		this.data = data;
+		let next;
+	}
+}
+
+class LinkList {
+	constructor() {
+		let first = null;
+	}
+
+	insertFirst(id, data) {
+		let newLink = new Link(id, data);
+		newLink.next = this.first;
+		this.first = newLink;
+	}
+
+	find(key) {
+		let current = this.first;
+		while (current.id != key) {
+			if (current.next == null) {
+				console.log(key + ' nety!');
+				return null;
+			}
+			else {
+				current = current.next;
+			}
+		}
+		console.log("Ya nashel' " + key + "!")
+		return current;
+	}
+
+	del(key) {
+		let current = this.first;
+		let previous = this.first;
+		while (current.id != key) {
+			if(current.next === null) {
+				return null;
+			}
+			else {
+				previous = current;
+				current = current.next;
+			}
+		}
+		if(current === this.first) {
+			this.first = this.first.next;
+		}
+		else {
+			previous.next = current.next;
+		}
+		console.log("Udalil' " + key);
+		return current;
+	}
+
+	displayList() {
+		console.log('theList')
+		let current = this.first;
+		while(current != null) {
+			console.log('id = ' + current.id + ';' + 'Data = ' + current.data);
+			current = current.next;
+		}
+	}
+}
+
+let theList = new LinkList();
+
+theList.insertFirst(1, 9);
+theList.insertFirst(2, 8);
+theList.insertFirst(3, 7);
+theList.insertFirst(4, 6);
+
+theList.displayList();
+
+theList.find(2);
+theList.find(12);
+
+theList.del(4);
+
+theList.displayList();
